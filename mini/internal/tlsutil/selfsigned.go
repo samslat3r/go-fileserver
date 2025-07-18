1ed3311f898c78e1593daf2244dc88c732b8db57
Sam Slater <sslater@posteo.net>
2025-07-18 15:43:58 -0700

    bigInt and Config functions didn't add on last push

--- mini/internal/tlsutil/selfsigned.go
+++ mini/internal/tlsutil/selfsigned.go
@@ -58,5 +58,22 @@ func generate(certPath, keyPath, host string) error {
 }
 
 func exists(p string) bool { _, err := os.Stat(p); return err == nil }
-
-
+func bigInt() *big.Int {
+	n, _ := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
+	return n
+}
+func Config() *tls.Config {
+	return &tls.Config{
+		MinVersion: tls.VersionTLS12,
+		CurvePreferences: []tls.CurveID{tls.CurveP512, tls.CurveP384, tls.CurveP256},
+		PreferCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_AES_256_GCM_SHA384, tls.TLS_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_EECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+		},
+	}
+}
